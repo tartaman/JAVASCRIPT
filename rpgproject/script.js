c9b2f345706dcd9b175956a3debf8409d357f5db
tartaman <popolol5342@gmail.com>
2021-03-21 03:27:36 -0600

    o c

---
+++ rpgproject/script.js
@@ -0,0 +1,17 @@
+//CHAMPECHE
+document.addEventListener("load", function ufas(){
+alert("eres Gladys, una bandida de un pueblo desconocido en la peninsula de yucatán el cual se llama CAMCHEPE")
+var bandidaONo = prompt("Dada tu situación economica tercermundista tienes que escoger entre ser una bandida de corazones o una bandida de riñones, escoge\n 1.Riñones \n 2.Corazones")
+if (bandidaONo == 1){
+    alert("has escogido robariñones")
+} else if (bandidaONo == 2){
+    alert("has escigido robacorazones")
+    alert("Como desearias")
+} else {
+    alert("wey sabes leer?")
+    ufas()
+}
+
+
+/////////////////////////////////////////////7
+})
