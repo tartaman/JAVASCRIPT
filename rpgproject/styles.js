d504509703f8c4ea32aab48d7bdc6a78fc2b6d77
tartaman <popolol5342@gmail.com>
2021-03-21 23:41:37 -0600

    ANIMACIONESSSSSSSSSSSSSSSSSSSSSS
    ASIES

---
+++ rpgproject/styles.js
@@ -0,0 +1,14 @@
+function jsjs(){
+    console.log("YA")
+    var button = document.getElementById("button")
+    button.innerHTML = "DALE CLICK YA"
+}
+
+setInterval(jsjs, 200)
+
+function jsjsjs() {
+    console.log("eh si we")
+    var button = document.getElementById("button")
+    button.innerHTML = "dale click para tener la aventura mas epica de tu vida wachin"
+}
+setInterval(jsjsjs, 400)
