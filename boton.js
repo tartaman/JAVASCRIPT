35b5a07114f9070d3531d7845a24480a2838ca98
tenientemantequilla13 <78754333+tenientemantequilla13@users.noreply.github.com>
2021-05-02 19:40:47 -0500

    15 minutos

---
+++ boton.js
@@ -0,0 +1,15 @@
+function cosa() {
+var boton = document.getElementById('ola');
+var texto1 = document.getElementById('A');
+
+
+    texto1.style.display = 'block'
+}
+
+function cosa2() {
+    var boton = document.getElementById('olaa');
+    var texto2 = document.getElementById('B');
+    
+    
+        texto2.style.display = 'block'
+    }
